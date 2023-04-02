4857b022e9c0a516a58d20b2fd15116e45461d85
jdgoes <diogowhite2003@gmail.com>
2023-04-02 20:08:37 -0300

    Exercicio-01

---
+++ Pratica01.java
@@ -0,0 +1,12 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+
+public class Pratica01{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Alô mundo !");
+        
+    }
+
+}
